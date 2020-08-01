fb5719f6762bd7de4f7f6a5d1431fc51341f3e79
Grunet <sadikshahidain@gmail.com>
2020-08-01 17:28:20 -0500

    Adding in a test to kill the "required" attribute line deletion mutant

--- test/e2e/createForm_test.js
+++ test/e2e/createForm_test.js
@@ -1,5 +1,14 @@
+import { Selector } from "testcafe";
+
 fixture`Form Tests`.page`localhost:3000/shell.html`;
 
 test("Fill out all fields and submit", async (t) => {
   await t.typeText("#username", "Some Username").click("input[type='submit']");
+});
+
+test("Fill out no fields and submit", async (t) => {
+  await t
+    .click("input[type='submit']")
+    .expect(Selector("form:invalid").exists)
+    .ok();
 });
