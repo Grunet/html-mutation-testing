5a91220dcef507922ac726e4f45ed36972e6d65d
Grunet <sadikshahidain@gmail.com>
2020-08-01 17:10:36 -0500

    Including line deletion mutations by reworking the js source a little

--- src/createForm.js
+++ src/createForm.js
@@ -1,19 +1,53 @@
-function prependForm(parentEl) {
-  const form = document.createElement("form");
+var form;
+function l1() {
+  form = document.createElement("form");
+}
+function l2() {
   form.setAttribute("action", "/shell.html");
-
-  const input = document.createElement("input");
+}
+var input;
+function l3() {
+  input = document.createElement("input");
+}
+function l4() {
   input.setAttribute("type", "text");
+}
+function l5() {
   input.setAttribute("id", "username");
+}
+function l6() {
   input.setAttribute("required", "");
+}
+function l7() {
   form.appendChild(input);
-
-  const submit = document.createElement("input");
+}
+var submit;
+function l8() {
+  submit = document.createElement("input");
+}
+function l9() {
   submit.setAttribute("type", "submit");
+}
+function l10() {
   form.appendChild(submit);
-
-  //Finishing attachment
-  parentEl.prepend(form);
+}
+//Finishing attachment
+function l11() {
+  document.body.prepend(form);
 }
 
-prependForm(document.body);
+function prependForm() {
+  l1();
+  l2();
+  l3();
+  l4();
+  l5();
+  l6();
+  l7();
+  l8();
+  l9();
+  l10();
+  l11();
+}
+
+prependForm();
